600b286db7f39a114856b283197012fc2ce323ee
Jaume Ramon <jaume_ramon@andorra.ad>
2025-01-03 12:24:47 +0100

    Proves Grid i List

--- src/app/components/movie-list-card/movie-list-card.component.ts
+++ src/app/components/movie-list-card/movie-list-card.component.ts
@@ -1,13 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { Collection } from 'src/app/models/collection.interface';
-import { Movie } from 'src/app/models/movie.interface';
+
+import {
+  animate,
+  query,
+  state,
+  style,
+  stagger,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-movie-list-card',
   templateUrl: './movie-list-card.component.html',
   styleUrls: ['./movie-list-card.component.css'],
+
+  animations: [
+    trigger('photosAnimation', [
+      state('void', style({ transform: 'scalex(500%) scaley(500%)' })),
+      transition('void <=> *', animate(1500)),
+    ]),
+  ],
 })
 export class MovieListCardComponent {
   @Input() collection: any;
-  displayedColumns: string[] = ['release_date', 'title', 'id'];
 }
--- src/app/components/movie-list-grid/movie-list-grid.component.ts
+++ src/app/components/movie-list-grid/movie-list-grid.component.ts
@@ -1,28 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Collection } from 'src/app/models/collection.interface';
-
-import {
-  animate,
-  query,
-  state,
-  style,
-  stagger,
-  transition,
-  trigger,
-} from '@angular/animations';
+import { Movie } from 'src/app/models/movie.interface';
 
 @Component({
   selector: 'app-movie-list-grid',
   templateUrl: './movie-list-grid.component.html',
   styleUrls: ['./movie-list-grid.component.css'],
-
-  animations: [
-    trigger('photosAnimation', [
-      state('void', style({ transform: 'scalex(500%) scaley(500%)' })),
-      transition('void <=> *', animate(1500)),
-    ]),
-  ],
 })
 export class MovieListGridComponent {
   @Input() collection: any;
+  displayedColumns: string[] = ['release_date', 'title', 'id'];
 }
